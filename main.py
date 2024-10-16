895f8e66a7609c38c858d7f0dc61e1b73a1531c9
Oringotoo <mac.ross@yandex.ru>
2024-10-16 10:09:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,11 @@
-import logging
-import requests
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
-
-# Логирование для отслеживания ошибок и событий
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
-
-# Функция обработки команды /start
-async def start(update: Update, context):
-    await update.message.reply_text(
-        "Привет! Отправь мне ссылку на мод с Steam Workshop, и я загружу его для тебя. Но хочу тебя предупредить, что из-за ограничений Telegram смогу закачать только файлы не более 50 МБ"
-    )
-
-# Функция обработки ссылок
 async def fetch_mod(update: Update, context):
     mod_url = update.message.text  # Получаем ссылку, которую прислал пользователь
     api_url = 'https://api.steamworkshopdownloader.ru/steam.request'
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Referer': 'https://yourwebsite.com'  # Укажите ваш реальный URL
     }
 
     try:
@@ -66,19 +49,3 @@ async def fetch_mod(update: Update, context):
 
     except requests.RequestException as e:
         await update.message.reply_text(f'Ошибка при получении данных: {e}')
-
-# Основная функция для запуска бота
-def main():
-    token = '7270313478:AAHHY7aghWwGUnOeQkURpcJYWtBV-KZRI60'  # Вставь сюда свой токен
-    app = ApplicationBuilder().token(token).build()
-
-    # Добавляем обработчики команд и сообщений
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, fetch_mod))
-
-    # Запускаем бота
-    app.run_polling()
-
-# Запуск бота
-if __name__ == '__main__':
-    main()
